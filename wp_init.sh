9d64cc1724e1b7ecff3b2ee046e4e9f28d7b17f3
Dirk Schumann <dirk@freunde-aus-berlin.net>
2022-02-13 13:21:42 +0100

    PoC PodloveAutoTest

--- wp_init.sh
+++ wp_init.sh
@@ -2,10 +2,12 @@ wp core install \
    --allow-root \
    --url="http://localhost:3001" \
    --title="My Blog" \
-   --admin_user="wordpress" \
-   --admin_password="wordpress" \
+   --admin_user="dirk" \
+   --admin_password="dirk" \
    --admin_email="your-email@example.com"
 
+wp --allow-root rewrite structure '/%postname%/'
 wp --allow-root plugin list
 wp --allow-root plugin activate podlove-publisher
+wp --allow-root plugin activate jwt-auth
 wp --allow-root plugin list
